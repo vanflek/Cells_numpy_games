af72431171dd16f5d447bcb5bb693d5e273f573d
vanflek <vanflek@gmail.com>
2025-07-20 22:06:27 -0700

    test

--- 2_3_Control_teleport.py
+++ 2_3_Control_teleport.py
@@ -8,9 +8,9 @@ TILE_X, TILE_Y = 32, 16
 
 WIDTH, HEIGHT = TILE_SIZE * TILE_X, TILE_SIZE * TILE_Y
 FPS = 60
-colors = {0:(0,0,0), 1:(255,255,255), 2:(255, 0,0), 3:(0,255,0), 4:(0,0,255), 5:(191,191,191)}
+colors = {0:(255,255,255), 1:(0,0,0), 2:(255, 0,0), 3:(0,255,0), 4:(0,0,255), 5:(191,191,191)}
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen_matrix = numpy.ones((WIDTH, HEIGHT), dtype=numpy.uint8)
+screen_matrix = numpy.zeros((WIDTH, HEIGHT), dtype=numpy.uint8)
 player_position = numpy.array((2,2), dtype=numpy.int8)
 screen_matrix[player_position[0], player_position[1]] = 3
 clock = pygame.time.Clock()
@@ -22,7 +22,7 @@ while run:
             run = False
         #control
         elif event.type == pygame.KEYDOWN:
-            screen_matrix[player_position[0],player_position[1]] = 1
+            screen_matrix[player_position[0],player_position[1]] = 0
             if event.key == pygame.K_UP:
                 player_position[0]-=1
             if event.key == pygame.K_DOWN:
@@ -39,7 +39,7 @@ while run:
     for y in range(TILE_Y):
         for x in range(TILE_X):
             pygame.draw.rect(screen, colors[screen_matrix[y,x]], (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
-            pygame.draw.rect(screen, colors[0], (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+            pygame.draw.rect(screen, colors[1], (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
     pygame.display.update()
     clock.tick(FPS)
 pygame.quit()
--- 2_4_Control_velocity.py
+++ 2_4_Control_velocity.py
@@ -8,9 +8,9 @@ TILE_X, TILE_Y = 32, 16
 
 WIDTH, HEIGHT = TILE_SIZE * TILE_X, TILE_SIZE * TILE_Y
 FPS = 60
-colors = {0:(0,0,0), 1:(255,255,255), 2:(255, 0,0), 3:(0,255,0), 4:(0,0,255), 5:(191,191,191)}
+colors = {0:(255,255,255), 1:(0,0,0), 2:(255, 0,0), 3:(0,255,0), 4:(0,0,255), 5:(191,191,191)}
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-screen_matrix = numpy.ones((WIDTH, HEIGHT), dtype=numpy.uint8)
+screen_matrix = numpy.zeros((WIDTH, HEIGHT), dtype=numpy.uint8)
 player_position = numpy.array((2,2), dtype=numpy.int8)
 player_velocity = numpy.array((0,1), dtype=numpy.int8)
 screen_matrix[player_position[0], player_position[1]] = 3
@@ -20,7 +20,6 @@ t_divider = 10
 key_permission = True
 run = True
 while run:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -44,7 +43,7 @@ while run:
     t += 1 if t < FPS-1 else - (FPS-1)
     if t%t_divider == 0:
         key_permission = True
-        screen_matrix[player_position[0], player_position[1]] = 1
+        screen_matrix[player_position[0], player_position[1]] = 0
         player_position += player_velocity
         player_position %= (TILE_Y, TILE_X)
         screen_matrix[player_position[0], player_position[1]] = 3
@@ -53,7 +52,7 @@ while run:
     for y in range(TILE_Y):
         for x in range(TILE_X):
             pygame.draw.rect(screen, colors[screen_matrix[y,x]], (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
-            pygame.draw.rect(screen, colors[0], (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+            pygame.draw.rect(screen, colors[1], (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
     pygame.display.update()
     clock.tick(FPS)
 pygame.quit()
